3f116dd3e1015533cd62a848766c814130963ba7
Lawrence Montaril <lawrencemontaril@gmail.com>
2025-12-01 03:43:55 +0800

    chore: redesign the welcome page

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -42,171 +42,169 @@ const filteredRightNavItems = computed(() => {
 </script>
 
 <template>
-    <div>
-        <div class="sticky top-0 z-50 border-b border-b-input bg-sidebar">
-            <div class="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
-                <!-- Mobile Menu -->
-                <div class="lg:hidden">
-                    <Sheet>
-                        <SheetTrigger :as-child="true">
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                class="mr-2 h-9 w-9"
-                            >
-                                <Menu class="h-5 w-5" />
-                            </Button>
-                        </SheetTrigger>
-
-                        <SheetContent
-                            side="left"
-                            class="w-[300px] p-6"
+    <div class="sticky top-0 z-50 border-b border-b-input bg-sidebar">
+        <div class="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
+            <!-- Mobile Menu -->
+            <div class="lg:hidden">
+                <Sheet>
+                    <SheetTrigger :as-child="true">
+                        <Button
+                            variant="ghost"
+                            size="icon"
+                            class="mr-2 h-9 w-9"
                         >
-                            <SheetTitle class="sr-only">Navigation Menu</SheetTitle>
+                            <Menu class="h-5 w-5" />
+                        </Button>
+                    </SheetTrigger>
 
-                            <SheetHeader class="flex justify-start text-left">
-                                <AppLogoIcon class="size-16 fill-current text-black dark:text-white" />
-                            </SheetHeader>
+                    <SheetContent
+                        side="left"
+                        class="w-[300px] p-6"
+                    >
+                        <SheetTitle class="sr-only">Navigation Menu</SheetTitle>
 
-                            <div class="flex h-full flex-1 flex-col justify-between space-y-4 py-6">
-                                <div class="flex flex-col space-y-4">
-                                    <Link
-                                        v-for="item in filteredRightNavItems"
-                                        :key="item.title"
-                                        :href="item.href"
-                                        class="flex items-center space-x-2 text-sm font-medium"
-                                        prefetch
-                                    >
-                                        <component
-                                            v-if="item.icon"
-                                            :is="item.icon"
-                                            class="h-5 w-5"
-                                        />
-                                        <span>{{ item.title }}</span>
-                                    </Link>
-                                </div>
+                        <SheetHeader class="flex justify-start text-left">
+                            <AppLogoIcon class="size-16 fill-current text-black dark:text-white" />
+                        </SheetHeader>
+
+                        <div class="flex h-full flex-1 flex-col justify-between space-y-4 py-6">
+                            <div class="flex flex-col space-y-4">
+                                <Link
+                                    v-for="item in filteredRightNavItems"
+                                    :key="item.title"
+                                    :href="item.href"
+                                    class="flex items-center space-x-2 text-sm font-medium"
+                                    prefetch
+                                >
+                                    <component
+                                        v-if="item.icon"
+                                        :is="item.icon"
+                                        class="h-5 w-5"
+                                    />
+                                    <span>{{ item.title }}</span>
+                                </Link>
                             </div>
-                        </SheetContent>
-                    </Sheet>
+                        </div>
+                    </SheetContent>
+                </Sheet>
+            </div>
+
+            <Link
+                :href="route('home')"
+                class="flex items-center gap-x-2"
+            >
+                <AppLogoIcon class="size-14" />
+                <span class="text-sm font-semibold text-white">Noynay Medical Center</span>
+            </Link>
+
+            <div class="ml-auto flex items-center space-x-2">
+                <div class="relative flex items-center space-x-1">
+                    <div class="hidden space-x-1 lg:flex">
+                        <template
+                            v-for="item in filteredRightNavItems"
+                            :key="item.title"
+                        >
+                            <TooltipProvider :delay-duration="0">
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            class="group h-9 w-9 cursor-pointer hover:bg-transparent"
+                                            as-child
+                                        >
+                                            <Link
+                                                :href="item.href"
+                                                prefetch
+                                            >
+                                                <span class="sr-only">{{ item.title }}</span>
+                                                <component
+                                                    :is="item.icon"
+                                                    class="size-5 text-white opacity-80 group-hover:opacity-100"
+                                                />
+                                            </Link>
+                                        </Button>
+                                    </TooltipTrigger>
+
+                                    <TooltipContent :side-offset="12">
+                                        <p>{{ item.title }}</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        </template>
+                    </div>
                 </div>
 
-                <Link
-                    :href="route('home')"
-                    class="flex items-center gap-x-2"
-                >
-                    <AppLogoIcon class="size-14" />
-                    <span class="text-sm font-semibold text-white">Noynay Medical Center</span>
-                </Link>
-
-                <div class="ml-auto flex items-center space-x-2">
-                    <div class="relative flex items-center space-x-1">
-                        <div class="hidden space-x-1 lg:flex">
-                            <template
-                                v-for="item in filteredRightNavItems"
-                                :key="item.title"
-                            >
-                                <TooltipProvider :delay-duration="0">
-                                    <Tooltip>
-                                        <TooltipTrigger>
-                                            <Button
-                                                variant="ghost"
-                                                size="icon"
-                                                class="group h-9 w-9 cursor-pointer hover:bg-transparent"
-                                                as-child
-                                            >
-                                                <Link
-                                                    :href="item.href"
-                                                    prefetch
-                                                >
-                                                    <span class="sr-only">{{ item.title }}</span>
-                                                    <component
-                                                        :is="item.icon"
-                                                        class="size-5 text-white opacity-80 group-hover:opacity-100"
-                                                    />
-                                                </Link>
-                                            </Button>
-                                        </TooltipTrigger>
-
-                                        <TooltipContent :side-offset="12">
-                                            <p>{{ item.title }}</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                            </template>
-                        </div>
-                    </div>
-
-                    <DropdownMenu v-if="auth.user">
-                        <DropdownMenuTrigger as-child>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                class="relative size-10 rounded-full"
-                            >
-                                <Avatar class="size-8 overflow-hidden rounded-full">
-                                    <AvatarImage
-                                        v-if="auth.user?.avatar"
-                                        :src="auth.user.avatar"
-                                        :alt="`${auth.user.first_name} ${auth.user.last_name}`"
-                                    />
-                                    <AvatarFallback
-                                        class="rounded-lg bg-neutral-200 font-semibold text-black dark:bg-neutral-700 dark:text-white"
-                                    >
-                                        {{ getInitials(`${auth.user.first_name} ${auth.user.last_name}`) }}
-                                    </AvatarFallback>
-                                </Avatar>
-                            </Button>
-                        </DropdownMenuTrigger>
-
-                        <DropdownMenuContent
-                            align="end"
-                            :side-offset="20"
-                            class="w-56"
+                <DropdownMenu v-if="auth.user">
+                    <DropdownMenuTrigger as-child>
+                        <Button
+                            variant="ghost"
+                            size="icon"
+                            class="relative size-10 rounded-full"
                         >
-                            <UserMenuContent :user="auth.user" />
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                    <div
-                        v-else
-                        class="flex items-center gap-2"
+                            <Avatar class="size-8 overflow-hidden rounded-full">
+                                <AvatarImage
+                                    v-if="auth.user?.avatar"
+                                    :src="auth.user.avatar"
+                                    :alt="`${auth.user.first_name} ${auth.user.last_name}`"
+                                />
+                                <AvatarFallback
+                                    class="rounded-lg bg-neutral-200 font-semibold text-black dark:bg-neutral-700 dark:text-white"
+                                >
+                                    {{ getInitials(`${auth.user.first_name} ${auth.user.last_name}`) }}
+                                </AvatarFallback>
+                            </Avatar>
+                        </Button>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent
+                        align="end"
+                        :side-offset="20"
+                        class="w-56"
                     >
-                        <Button
-                            v-if="!route().current('login')"
-                            variant="secondary"
-                            as-child
+                        <UserMenuContent :user="auth.user" />
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <div
+                    v-else
+                    class="flex items-center gap-2"
+                >
+                    <Button
+                        v-if="!route().current('login')"
+                        variant="secondary"
+                        as-child
+                    >
+                        <Link
+                            :href="route('login')"
+                            prefetch
                         >
-                            <Link
-                                :href="route('login')"
-                                prefetch
-                            >
-                                Log in
-                            </Link>
-                        </Button>
+                            Log in
+                        </Link>
+                    </Button>
 
-                        <Button
-                            v-if="!route().current('register')"
-                            class="border border-primary-foreground dark:border-primary"
-                            as-child
+                    <Button
+                        v-if="!route().current('register')"
+                        class="border border-primary-foreground dark:border-primary"
+                        as-child
+                    >
+                        <Link
+                            :href="route('register')"
+                            prefetch
                         >
-                            <Link
-                                :href="route('register')"
-                                prefetch
-                            >
-                                Register
-                            </Link>
-                        </Button>
-                    </div>
+                            Register
+                        </Link>
+                    </Button>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div
-            v-if="props.breadcrumbs.length > 1"
-            class="flex w-full border-b border-sidebar-border/70"
-        >
-            <div class="mx-auto flex h-12 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
-                <Breadcrumbs :breadcrumbs="breadcrumbs" />
-            </div>
+    <div
+        v-if="props.breadcrumbs.length > 1"
+        class="flex w-full border-b border-sidebar-border/70"
+    >
+        <div class="mx-auto flex h-12 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
+            <Breadcrumbs :breadcrumbs="breadcrumbs" />
         </div>
     </div>
 </template>
--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,24 +1,228 @@
 <script setup lang="ts">
 import Button from '@/components/ui/button/Button.vue';
-import AuthBase from '@/layouts/AuthLayout.vue';
 import LandingLayout from '@/layouts/LandingLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const services = {
+    consultation: {
+        label: 'Consultation',
+        children: [{ label: 'Consultation', value: 'consultation' }],
+    },
+    family_planning_service: {
+        label: 'Family Planning Services',
+        children: [
+            { label: 'Family Planning Counseling', value: 'family_planning_counseling' },
+            { label: 'Natural Methods (Rhythm), Pills, Depotrust', value: 'natural_methods' },
+        ],
+    },
+    integrative_and_wellness: {
+        label: 'Integrative and Wellness Healthcare Services',
+        children: [
+            { label: 'Chelation Therapy', value: 'chelation_therapy' },
+            { label: 'Magnetic Resonance Analysis', value: 'magnetic_resonance_analysis' },
+            {
+                label: 'Multifunctional High Potential Therapeutic Services',
+                value: 'multifunctional_high_potential_therapeutic_services',
+            },
+            { label: 'Weight Loss Management', value: 'weight_loss_management' },
+            { label: 'Psychosocial and Spiritual Counseling', value: 'psychosocial_and_spiritual_counseling' },
+        ],
+    },
+    laboratory_services: {
+        label: 'Laboratory Services',
+        children: [
+            { label: 'Pregnancy Test', value: 'pregnancy_test' },
+            { label: 'Papsmear', value: 'papsmear' },
+            { label: 'Complete Blood Count', value: 'cbc' },
+            { label: 'Urinalysis', value: 'urinalysis' },
+            { label: 'Fecalysis', value: 'fecalysis' },
+        ],
+    },
+    maternal_and_child_health_services: {
+        label: 'Maternal and Child Health Services',
+        children: [
+            { label: 'Pre-Natal and Post-Natal Check Up', value: 'pre_natal_and_post_natal' },
+            { label: 'Normal Spontaneous Delivery', value: 'normal_spontaneous_delivery' },
+            { label: 'Immunization - BCG, HEP. B Vaccines, etc.', value: 'immunization' },
+            { label: 'Ear Piercing With Hypoallergenic Earrings', value: 'ear_pearcing' },
+            { label: 'Nebulization With and Without Medication', value: 'nebulization' },
+            { label: 'Foley Catheter Insertion', value: 'foley_catheter_insertion' },
+            { label: 'Surgical Wound Dressing', value: 'surgical_wound_dressing' },
+            { label: 'Cord Dressing', value: 'cord_dressing' },
+            { label: 'Suture Removal', value: 'suture_removal' },
+            { label: 'Issuance of Birth Certificate; Newborn Screening', value: 'issuance_of_bc_newborn_screening' },
+        ],
+    },
+    medical_surgical_services: {
+        label: 'Medical/Surgical Services',
+        children: [
+            { label: 'General OPD Consultation', value: 'general_opd_consultation' },
+            { label: 'Medical / OPD / Pre-Employment Consultations', value: 'medical_opd_consultation' },
+            { label: 'Minor Surgical Procedures', value: 'minor_surgical_procedures' },
+            { label: 'Issuance of Medical Certificate', value: 'issuance_of_medical_certificate' },
+            { label: 'Pedia / Adult Immunization / Vaccination Services', value: 'pedia_adult_vaccination_services' },
+        ],
+    },
+};
+
+type ServiceKey = keyof typeof services;
+
+const categoryRefs = ref<Record<ServiceKey, HTMLElement | null>>({
+    consultation: null,
+    family_planning_service: null,
+    integrative_and_wellness: null,
+    laboratory_services: null,
+    maternal_and_child_health_services: null,
+    medical_surgical_services: null,
+});
+
+const scrollToCategory = (key: ServiceKey) => {
+    const element = categoryRefs.value[key];
+    if (element) element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+};
+
+const setCategoryRef = (key: ServiceKey, el: HTMLElement | null) => {
+    categoryRefs.value[key] = el;
+};
+
+const activeSection = ref<ServiceKey | null>(null);
+let observer: IntersectionObserver;
+
+onMounted(() => {
+    const sections = Object.entries(categoryRefs.value)
+        .map(([key, el]) => ({ key: key as ServiceKey, el }))
+        .filter((s) => s.el !== null) as { key: ServiceKey; el: HTMLElement }[];
+
+    observer = new IntersectionObserver(
+        () => {
+            let closestSection: { key: ServiceKey; distance: number } | null = null;
+
+            sections.forEach(({ key, el }) => {
+                const rect = el.getBoundingClientRect();
+                const distance = Math.abs(rect.top); // distance from top of viewport
+
+                if (!closestSection || distance < closestSection.distance) {
+                    closestSection = { key, distance };
+                }
+            });
+
+            if (closestSection) {
+                activeSection.value = closestSection.key;
+            }
+        },
+        { threshold: [0, 0.25, 0.5, 0.75, 1] },
+    );
+
+    sections.forEach(({ el }) => observer.observe(el));
+});
+
+onUnmounted(() => {
+    if (observer) observer.disconnect();
+});
 </script>
 
 <template>
     <Head title="Home" />
 
     <LandingLayout>
-        <AuthBase
-            title="We serve to make you better"
-            description="We are open 24/7"
-        >
+        <!-- Hero Section -->
+        <section class="bg-blue-50 py-16 text-center">
+            <h1 class="mb-4 text-4xl font-bold">We serve to make you better.</h1>
+            <p class="mb-6 text-lg text-gray-700">
+                Book appointments, manage billing, and access healthcare services anytime, anywhere.
+            </p>
+
             <Button
                 size="lg"
                 as-child
             >
-                <Link :href="route('register')"> Get started </Link>
+                <Link
+                    :href="route('register')"
+                    prefetch
+                >
+                    Get Started
+                </Link>
             </Button>
-        </AuthBase>
+        </section>
+
+        <!-- Services Section -->
+        <section class="mt-12 flex px-4">
+            <!-- Navigation Sidebar -->
+            <nav class="sticky top-20 hidden h-[80vh] w-64 overflow-auto pr-4 md:block">
+                <h2 class="mb-4 text-xl font-bold">Jump to Service</h2>
+                <ul>
+                    <li
+                        v-for="(service, key) in services"
+                        :key="key"
+                        class="mb-2"
+                    >
+                        <Button
+                            class="h-fit w-full wrap-break-word whitespace-normal"
+                            @click="scrollToCategory(key)"
+                            :variant="activeSection === key ? 'default' : 'ghost'"
+                        >
+                            {{ service.label }}
+                        </Button>
+                    </li>
+                </ul>
+            </nav>
+
+            <!-- Services List -->
+            <div class="flex-1 space-y-12">
+                <div
+                    v-for="(service, key) in services"
+                    :key="key"
+                    :ref="(el) => setCategoryRef(key as ServiceKey, el as HTMLElement | null)"
+                    :data-key="key"
+                    class="scroll-m-16"
+                >
+                    <h3 class="sticky top-0 z-10 border-b border-gray-200 bg-white px-2 py-3 text-2xl font-bold">
+                        {{ service.label }}
+                    </h3>
+
+                    <div class="mt-4 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                        <div
+                            v-for="child in service.children"
+                            :key="child.value"
+                            class="overflow-hidden rounded-lg bg-white shadow-md"
+                        >
+                            <img
+                                :src="`/images/services/${child.value}.jpg`"
+                                :alt="child.label"
+                                class="h-48 w-full object-cover"
+                            />
+                            <div class="p-4">
+                                <h4 class="mb-2 text-lg font-semibold">{{ child.label }}</h4>
+                                <p class="text-sm text-gray-600">
+                                    Comprehensive care and expert attention to support your health and wellness.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Call to Action -->
+        <section class="mt-16 bg-gray-50 py-16 text-center">
+            <h2 class="mb-4 text-2xl font-bold">Why Choose Us?</h2>
+            <p class="mx-auto mb-6 max-w-2xl text-gray-700">
+                Our clinic combines advanced medical technology with personalized care. Manage appointments, track
+                billing, and access specialized services all in one place—designed for your convenience.
+            </p>
+
+            <Button
+                size="lg"
+                as-child
+            >
+                <Link
+                    :href="route('register')"
+                    prefetch
+                >
+                    Book Your Appointment
+                </Link>
+            </Button>
+        </section>
     </LandingLayout>
 </template>
